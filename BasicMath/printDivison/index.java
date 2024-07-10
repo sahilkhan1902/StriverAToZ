0292a28c8921cc1d812e74bed7cde7ab945cf484
sahil khan <sahilkhan2002786@gmail.com>
2024-07-10 22:50:01 +0530

    optimal solution for print divison

--- BasicMath/printDivison/index.java
+++ BasicMath/printDivison/index.java
@@ -1,14 +1,18 @@
 package BasicMath.printDivison;
 
 public class index {
-    public static void printDivison(int n){
-        for (int i = 1; i <= n; i++) {
-            if(n%i == 0){
+    public static void printDivision(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 System.out.println(i);
+                if (i != n / i) { // Check to avoid printing the square root twice
+                    System.out.println(n/i);
+                }
             }
         }
     }
-   public static void main(String[] args) {
-    printDivison(12);
-   } 
+
+    public static void main(String[] args) {
+        printDivision(12);
+    }
 }
